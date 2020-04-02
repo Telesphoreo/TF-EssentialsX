134f48366f3a6992c875a7a4915521200a457bf8
Telesphoreo <me@telesphoreo.me>
2020-04-01 23:13:36 -0700

    d

--- Essentials/src/com/earth2me/essentials/commands/Commandafk.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandafk.java
@@ -2,6 +2,7 @@ package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.CommandSource;
 import com.earth2me.essentials.User;
+import me.totalfreedom.essentials.Handler;
 import net.ess3.api.events.AfkStatusChangeEvent;
 import org.bukkit.Server;
 
@@ -18,7 +19,7 @@ public class Commandafk extends EssentialsCommand {
 
     @Override
     public void run(Server server, User user, String commandLabel, String[] args) throws Exception {
-        if (args.length > 0 && user.isAuthorized("essentials.afk.others")) {
+        if (args.length > 0 && Handler.isAdmin(user.getBase())) {
             User afkUser = user; // if no player found, but message specified, set command executor to target user
             String message;
             try {
@@ -81,7 +82,7 @@ public class Commandafk extends EssentialsCommand {
 
     @Override
     protected List<String> getTabCompleteOptions(Server server, User user, String commandLabel, String[] args) {
-        if (args.length == 1 && user.isAuthorized("essentials.afk.others")) {
+        if (args.length == 1 && Handler.isAdmin(user.getBase())) {
             return getPlayers(server, user);
         } else {
             return Collections.emptyList();
